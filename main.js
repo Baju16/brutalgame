54882e18fd1fa5f48e27ec44e6b905ad81f7d5c1
Dawid Bajek <dawidbajek03g@gmail.com>
2022-12-24 11:35:34 +0100

    add files

---
+++ main.js
@@ -0,0 +1,9 @@
+const score = document.getElementById("score");
+const turnNumber = document.getElementById("number_turn");
+const currentTurn = document.getElementById("current_turn");
+
+const abilities = [...document.getElementsByClassName("ability")]; 
+const playerHp = document.getElementById("p_healthpoints"); 
+const aiHp = document.getElementById("ai_healthpoints"); 
+
+
